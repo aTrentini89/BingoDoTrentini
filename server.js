233f6733e63b812dbd77b95ed7a3119fc1c0d2ce
aTrentini89 <teratecstore@gmail.com>
2024-12-26 00:42:27 -0300

    mensagem

--- server.js
+++ server.js
@@ -5,17 +5,28 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 // Serve os arquivos estáticos da pasta atual
 app.use(express.static(__dirname));
 
+// Adiciona rota básica para teste
+app.get('/health', (req, res) => {
+  res.send('Server is running');
+});
+
 const games = new Map();
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
+  console.log('New client connected:', socket.id);
 
   socket.on('createGame', (data) => {
+    console.log('Creating game:', data);
     const { gameCode, adminName, bingoName } = data;
     games.set(gameCode, {
       bingoName,
@@ -30,23 +41,27 @@ io.on('connection', (socket) => {
   });
 
   socket.on('joinGame', (data) => {
+    console.log('Player joining game:', data);
     const { gameCode, playerName } = data;
     const game = games.get(gameCode);
     if (game) {
       game.players.push({ name: playerName, active: true, status: 'Jogador' });
       socket.join(gameCode);
       io.to(gameCode).emit('gameState', game);
+    } else {
+      socket.emit('error', { message: 'Game not found' });
     }
   });
 
   socket.on('updateGame', (data) => {
+    console.log('Updating game:', data);
     const { gameCode, gameState } = data;
     games.set(gameCode, gameState);
     io.to(gameCode).emit('gameState', gameState);
   });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log('Client disconnected:', socket.id);
   });
 });
 
